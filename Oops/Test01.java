03e196e89886d4e8caf2ec9b33cec166f7ddc4e0
manish <108730254+Manish142219@users.noreply.github.com>
2023-11-04 18:00:41 +0530

    Update Test01.java

--- Oops/Test01.java
+++ Oops/Test01.java
@@ -1,1 +1,36 @@
+public class AbstractionUse {
 
+  // here we are using the abstraction class 
+  
+    public static void main(String[] args) {
+
+        Horse h = new Horse();
+        h.walk();
+
+        Chicken ck = new Chicken();
+        ck.walk();
+
+    }
+
+}
+
+abstract class Animal {
+    void eat() {
+        System.out.println("animal can be eat what they wants ");
+    }
+
+    abstract void walk();
+}
+
+class Horse extends Animal {
+    void walk() {
+        System.out.println("Horse can be walk with his 4 legs");
+    }
+
+}
+
+class Chicken extends Animal {
+    void walk() {
+        System.out.println("Chicken cane be walk with his 2 legs ");
+    }
+}
