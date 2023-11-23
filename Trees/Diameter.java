317018db8fb34c25563c599dbc87ff657965f2b0
manish <108730254+Manish142219@users.noreply.github.com>
2023-11-23 18:02:42 +0530

    Create Diameter.java

---
+++ Trees/Diameter.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+
+// here we are printing the Diameter of the root 
+
+public class DiameterOfTree {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        // making the Constructor
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+
+        }
+    }
+
+    public static int height(Node root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int leftHt = height(root.left);
+        int rightHt = height(root.right);
+
+        return Math.max(leftHt, rightHt) + 1;
+    }
+
+    public static int diameter(Node root) {
+        if (root == null) {
+            return 0;
+
+        }
+
+        int leftDia = diameter(root.left);
+        int leftHt = height(root.left);
+        int rightDia = diameter(root.right);
+        int rightHt = height(root.right);
+
+        int selfDia = leftHt + rightHt + 1;
+        return Math.max(selfDia, Math.max(rightDia, leftDia));
+    }
+    
+    public static void main(String[] args) {
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(diameter(root));
+    }
+
+
+
+}
