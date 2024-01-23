6bbbecc46cf2dca0beb43bcd89446912a872ab95
manish <108730254+Manish142219@users.noreply.github.com>
2024-01-24 02:13:27 +0530

    Update PowerOfNumber.java

--- Recursion/PowerOfNumber.java
+++ Recursion/PowerOfNumber.java
@@ -2,12 +2,29 @@ public class PowerOf {
 
     // here we are calculating the power of a number!
 
-    public static int calculate(int n, int pow){
+    // public static int calculate(int n, int pow){
+    //     if(pow == 0 ){
+    //         return 1;
+    //     }
+    //     return n * calculate(n, pow-1);
+    // }
+
+    // optimized approach
+    public static int calculate(int n , int pow){
         if(pow == 0 ){
             return 1;
         }
-        return n * calculate(n, pow-1);
+        if(pow % 2 == 0){
+            //means power is odd
+            return calculate(n, pow/2)*calculate(n,pow/2);
+        }
+        else{
+            //power is odd
+            return n * calculate(n, pow/2)*calculate(n,pow/2);
+        }
     }
+
+    
     public static void main(String[] args) {
         int n = 2;
         int pow = 10;
