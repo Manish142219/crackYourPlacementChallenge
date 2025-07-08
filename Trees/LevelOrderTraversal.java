bb947544e483b0114fc32178ed99faf08918c1c1
manish <108730254+Manish142219@users.noreply.github.com>
2025-07-08 10:11:49 +0530

    Create LevelOrderTraversal.java

---
+++ Trees/LevelOrderTraversal.java
@@ -0,0 +1,58 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class LevelOrderTraversal {
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    // pring the elements levelwise
+    public static void printLevel(Node root) {
+        if (root == null) {
+            return;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if (currNode == null) {
+                // printing the next line
+                System.out.println();
+                if (q.isEmpty()) {
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                System.out.print(currNode.data);
+                if (currNode.left != null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right != null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.left.left = new Node(3);
+        root.left.right = new Node(5);
+        root.right = new Node(6);
+        root.right.left = new Node(7);
+
+        printLevel(root);
+
+    }
+}
